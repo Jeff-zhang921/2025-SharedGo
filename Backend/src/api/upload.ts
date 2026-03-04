2ab68230069e03da780bb8fb700c991211231ef8
unknown <ub24928@bristol.ac.uk>
2026-03-04 14:55:35 +0000

    update upload route

--- Backend/src/api/upload.ts
+++ Backend/src/api/upload.ts
@@ -6,6 +6,7 @@ const utapi = new UTApi();
 
 const MAX_IMAGE_UPLOAD_BYTES = 10 * 1024 * 1024;
 //multer Populates req.file(neat JavaScript Object)
+//When you use multer({ storage: multer.memoryStorage() }), it automatically adds a property(req.file) to the  (request) object
 const upload = multer({
     //No File Created: The file is never written to your server's uploads/ folder.
     //The "Buffer": The file data is stored in a Buffer object inside req.file.buffer.
@@ -14,6 +15,7 @@ const upload = multer({
 });
 
 const router=Router()
+//workflow:assign the type of file,, use UTFile change them to format that UploadThing API can accept, then call the API to upload the file, and get the upload.data which contain the url of the file by calling .ufsUrl
 router.post("/upload-image",upload.single("file"),async (req:Request,res:Response)=>{
     if (!req.file){
         res.status(400).json({message:"No file uploaded"})
@@ -39,13 +41,37 @@ router.post("/upload-image",upload.single("file"),async (req:Request,res:Respons
     return 
    }
    try{
-    if (isImage){
-      
+    if (isVideo||isImage){
+      //new UTFile(parts, name, options)
+      //Multer’s in-memory data to “byte array”
+      const uploadFile=new UTFile([file.buffer as BlobPart],file.originalname||`chat${Date.now()}`,{
+      type:file.mimetype,
+      lastModified:Date.now()
+      })
+      //Calls UploadThing’s API to upload the file
+      const upload=await utapi.uploadFiles(uploadFile)
+      //upload can return a single result object (if you pass one file), or array of result objects (if you pass multiple files)
+      const uploadedData=Array.isArray(upload)?upload[0]?.data:upload.data
+      const uploadedErr=Array.isArray(upload)?upload[0]?.error:upload.error
+      if(uploadedErr||!uploadedData){
+        console.error("UploadThing API error:", uploadedErr);
+        res.status(500).json({ message: "Failed to upload file" });
+        return
+      }
+     const url=uploadedData.ufsUrl??uploadedData.url
+     if (!url){
+        console.error("UploadThing API error: No URL returned");
+        res.status(500).json({ message: "Failed to upload file" });
+        return
+     }
+      res.json({url})
+
     }
-   }catch (err){
+  }catch (err){
     console.error("Upload error:", err);
     res.status(500).json({ message: "Failed to upload file" });
    }
+   
   })
 
 
