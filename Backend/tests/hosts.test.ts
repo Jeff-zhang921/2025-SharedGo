478ce1454b1905bf853faa99605dde454e387fa9
Soko <az23290@bristol.ac.uk>
2026-03-14 20:01:33 +0000

    initialize mock datas

---
+++ Backend/tests/hosts.test.ts
@@ -0,0 +1,94 @@
+import request from "supertest";
+import express from "express";
+
+const mockPrisma = {
+  user: {
+    upsert: jest.fn(),
+    findUnique: jest.fn(),
+  },
+  event: {
+    create: jest.fn(),
+    findUnique: jest.fn(),
+    findMany: jest.fn(),
+    count: jest.fn(),
+  },
+  review: {
+    findMany: jest.fn(),
+    count: jest.fn(),
+  },
+  $disconnect: jest.fn(),
+  
+};
+
+//define Enum for the mock
+const mockCategory = {
+  PHYSICAL_ACTIVITIES: "Physical Activities",
+  FESTIVALS: "Festivals",
+  EDUCATIONAL: "Educational",
+  NETWORKING: "Networking",
+  ARTS_CULTURE: "Arts & Culture",
+  FOOD_DRINK: "Food & Drink",
+  MUSIC_CONCERTS: "Music & Concerts",
+  TECH_GAMING: "Tech & Gaming",
+  WELLNESS_MEDITATION: "Wellness & Meditation",
+  VOLUNTEER_CHARITY: "Volunteer & Charity",
+  OTHER: "Other"
+};
+// Replace the real PrismaClient export with our mock implementation.
+jest.mock("@prisma/client", () => ({
+  PrismaClient: jest.fn(() => mockPrisma),
+  Category: mockCategory,
+}));
+
+import app from "../src";
+import hostsRouter from "../src/routes/hosts";
+app.use(express.json());
+app.use("/hosts", hostsRouter);
+
+const hostId = 1;
+
+const mockHost = {
+    id: hostId,
+    name: "Host",
+    email: "host@gmail.com",
+};
+
+const mockEvent = {
+      id: 123,
+      title: "Test Event",
+      description: "This is a test event",
+      startsAt: new Date("2024-12-31T22:00:00.000Z"),
+      capacity: 50,
+      category: "Other",
+      location: "Test Location",
+      latitude: 0,
+      longitude: 0,
+      hostId: 1,
+      host: {
+        id: 1,
+        email: "host@example.com",
+        name: null,
+      },
+      participants: [],
+      reviews: [],
+};
+
+const mockReview = {
+    id: 1,
+    rating: 3,
+    comment: "I really enjoyed it!",
+    eventId: 123,
+    hostId: 1,
+    authorid: 100,
+    createdAt: new Date("2026-03-01T10:00:00Z"),
+};
+
+beforeAll(() => {
+    // Clear all mocks before starting tests
+    jest.clearAllMocks();
+});
+
+afterAll(() => {
+    jest.resetAllMocks();
+}); 
+
